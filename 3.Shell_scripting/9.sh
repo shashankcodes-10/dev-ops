72615617d2e97bfcc3091f360a6b484a8fc6b3bd
shashank pipal <u10shashank@gmail.com>
2026-06-19 18:21:07 +0530

    new file added

---
+++ 3.Shell_scripting/9.sh
@@ -0,0 +1,31 @@
+# Challenge #9
+# Problem Statement
+
+# Write a shell script that prints numbers from 1 to 10.
+
+# Expected Output
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# Requirements
+# Use a for loop
+# No while
+# No user input
+# No if-else
+# No case
+
+
+#!/bin/bash
+
+
+for i in {1..10} 
+do
+	echo "$i"
+done
