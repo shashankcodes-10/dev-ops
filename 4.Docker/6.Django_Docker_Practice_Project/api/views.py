673a327450868dde17592c8e279ccbcf15707484
shashank pipal <u10shashank@gmail.com>
2026-06-19 12:30:19 +0530

    cleaning

--- 4.Docker/6.Django_Docker_Practice_Project/api/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def health(request):
-    return HttpResponse("OK")
--- 4.Docker/6.Django_Docker_Practice_Project/manage.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python
-print('Create project with django-admin startproject notesapp .')
