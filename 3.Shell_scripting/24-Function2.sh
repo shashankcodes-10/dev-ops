4244f1dc7496174b56dee8f2ba6db0a11fccb84e
shashank pipal <u10shashank@gmail.com>
2026-06-20 17:06:42 +0530

    adding new file

---
+++ 3.Shell_scripting/24-Function2.sh
@@ -0,0 +1,28 @@
+# Next Challenge (Functions + Arguments)
+
+# Write a function:
+
+# greet()
+
+# The function should accept a name as an argument and print:
+
+# Hello Shashank
+
+# Example:
+
+# greet "Shashank"
+
+# Output:
+
+# Hello Shashank
+
+
+#!/bin/bash
+
+greet() {
+
+   echo "Hello $1"
+}
+
+greet "shashank"
+
