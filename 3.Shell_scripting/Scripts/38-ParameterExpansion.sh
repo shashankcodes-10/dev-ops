97c73522ceac4bfd9bc9af3a3fe96071cb7d86b5
shashank pipal <u10shashank@gmail.com>
2026-06-22 17:35:42 +0530

    adding new file

---
+++ 3.Shell_scripting/Scripts/38-ParameterExpansion.sh
@@ -0,0 +1,104 @@
+1. Get Variable Value
+name="shashank"
+
+echo ${name}
+
+Output:
+
+shashank
+
+
+2. Length of Variable
+name="shashank"
+
+echo ${#name}
+
+Output:
+
+8
+
+Very useful.
+
+
+3. Remove From End (Shortest Match)
+file="app.log"
+
+echo ${file%.log}
+
+Output:
+
+app
+
+Used often.
+
+
+4. Remove From Start (Shortest Match)
+path="/home/shashank/file.txt"
+
+echo ${path#/home/}
+
+Output:
+
+shashank/file.txt
+
+
+5. Replace Text
+env="dev"
+
+echo ${env/dev/prod}
+
+Output:
+
+prod
+
+
+6. Default Value
+echo ${name:-Guest}
+
+If:
+
+name=""
+
+Output:
+
+Guest
+
+
+7. Substring
+name="shashank"
+
+echo ${name:0:4}
+
+Output:
+
+shas
+
+
+8. Uppercase
+name="shashank"
+
+echo ${name^^}
+
+Output:
+
+SHASHANK
+
+
+9. Lowercase
+name="SHASHANK"
+
+echo ${name,,}
+
+Output:
+
+shashank
+
+
+10. Remove Last Character
+disk="42%"
+
+echo ${disk%\%}
+
+Output:
+
+42
