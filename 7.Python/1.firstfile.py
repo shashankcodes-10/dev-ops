ee6e2702f3f78fc48fae945c72fedc3d76f54b6e
shashank pipal <u10shashank@gmail.com>
2026-06-23 18:00:00 +0530

    adding python folder

---
+++ 7.Python/1.firstfile.py
@@ -0,0 +1,35 @@
+#this is a comment
+
+print("hello world")
+
+#100 = constant
+#yes = constant
+
+output = "yes" # variable
+value= 100 # variable
+
+#   operator
+
+a = 200
+b = 100
+
+c =a+b
+d =a-b
+e =a*b
+
+print(c , d , e)
+
+
+
+#Data types
+
+# int = 1 2 100 2676
+#float = 1.2 -2.4
+#boolean = true false
+#string = "yes" "hello"
+
+val = "true"
+print(type(a)) # to get data type
+print(type(val))
+
+ 
