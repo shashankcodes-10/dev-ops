6c4bf3970d0ecd1433c0d2f5491ffed59d1213ce
shashank pipal <u10shashank@gmail.com>
2026-06-24 18:00:00 +0530

    new file

---
+++ 7.Python/2.Check_env.py
@@ -0,0 +1,31 @@
+# Get Environment from user
+
+env = input("Enter the Environmrnt : ")
+
+print( "the Environment is  : " ,env)
+
+
+# Conditional statement
+
+if env =="prd":
+   print("Don't Deply on friday")
+
+elif env =="stg":
+   print("Take backup and do testing")
+
+else:
+    print("save to deply")
+
+# Type Casting
+ 
+num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+
+
+print("the sum is",num1+num2)
+print("the subtraction is",num1-num2)
+print("the multiplication is",num1*num2)
+print("the divsion is",num1/num2)
+
+#operator
+# == <=  >= != 
