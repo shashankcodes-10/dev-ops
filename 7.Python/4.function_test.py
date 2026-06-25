06dfb75839baa32ee5401e4b4367d0aea6173d7a
shashank pipal <u10shashank@gmail.com>
2026-06-25 13:48:26 +0530

    added new file

---
+++ 7.Python/4.function_test.py
@@ -0,0 +1,13 @@
+#function
+
+
+def sum_of_num():   # function definition
+    num1 = int(input("Enter the first num : "))
+    num2 = int(input("Enter the second num : "))
+
+    sum=num1+num2
+    print(f"sum is {sum}")
+
+
+
+sum_of_num()   # function calling
