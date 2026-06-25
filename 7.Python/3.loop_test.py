19ae91ac1829f86bda124437b3f0186055911b5f
shashank pipal <u10shashank@gmail.com>
2026-06-25 13:37:00 +0530

    adding new file

--- 7.Python/3.loop_test.py
+++ 7.Python/3.loop_test.py
@@ -1,6 +1,19 @@
 #loop 
 
+#print value between 0 and 5
 for i in range(5):
   print(i)
-  print("hello")
-  
+
+#print value from 1 to 5
+for v in range(1,6):
+  # formatted string
+  print(f"the valus of v is {v}")
+
+
+
+# while loop
+num =1
+
+while num <= 5:
+     print(num)
+     num+= 1
