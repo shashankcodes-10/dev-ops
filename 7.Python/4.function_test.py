d4bbe2e9e3d1b1d45f2cdfc1ad5840080ca7438c
shashankcodes-10 <124920174+shashankcodes-10@users.noreply.github.com>
2026-06-26 12:31:27 +0530

    Update 4.function_test.py

--- 7.Python/4.function_test.py
+++ 7.Python/4.function_test.py
@@ -2,8 +2,8 @@
 
 
 def sum_of_num():   # function definition
-    num1 = int(input("Enter the first num : "))
-    num2 = int(input("Enter the second num : "))
+    num1 = int(input("Enter the firstNum : "))
+    num2 = int(input("Enter the secondNum : "))
 
     sum=num1+num2
     print(f"sum is {sum}")
